6be4fe76dc6ba107a35a4befb0fc893310c95e35
Matt Thomas <mthomas@guggenheim.org>
2019-06-29 22:55:42 -0400

    Adds featured category customizer setting

--- functions.php
+++ functions.php
@@ -1,26 +1,29 @@
 <?php
 // wp_enqueue_style( 'style', get_stylesheet_uri() );
 // wp_enqueue_style( 'slider', get_template_directory_uri() . '/css/slider.css',false,'1.1','all');
-
 function harwintonfair_widgets() {
 
-	register_sidebar( array(
-		'name'          => 'Call To Action',
-		'id'            => 'call_to_action',
-		'before_widget' => '<div>',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h2 class="rounded">',
-		'after_title'   => '</h2>',
-    ) );
-    
-    register_sidebar( array(
-		'name'          => 'Main Menu',
-		'id'            => 'main_menu',
-		'before_widget' => '<div>',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h2 class="rounded">',
-		'after_title'   => '</h2>',
-	) );
+	register_sidebar(
+		array(
+			'name'          => 'Call To Action',
+			'id'            => 'call_to_action',
+			'before_widget' => '<div>',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h2 class="rounded">',
+			'after_title'   => '</h2>',
+		)
+	);
+
+	register_sidebar(
+		array(
+			'name'          => 'Main Menu',
+			'id'            => 'main_menu',
+			'before_widget' => '<div>',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h2 class="rounded">',
+			'after_title'   => '</h2>',
+		)
+	);
 
 }
 add_action( 'widgets_init', 'harwintonfair_widgets' );
@@ -51,13 +54,19 @@ if ( ! function_exists( 'harwintonfair_setup' ) ) {
 add_action( 'after_setup_theme', 'harwintonfair_setup' );
 
 /**
- * Register customizer settings/controls. 
+ * Register customizer settings/controls.
  */
 function harwintonfair_customize_register( $wp_customize ) {
 	// add color picker setting
 	$wp_customize->add_setting(
 		'bar_color', array(
 			'default' => '#ff0000',
+		)
+	);
+
+	$wp_customize->add_setting(
+		'featured_category', array(
+			'default' => '',
 		)
 	);
 
@@ -71,6 +80,25 @@ function harwintonfair_customize_register( $wp_customize ) {
 			)
 		)
 	);
+
+	$wp_customize->add_control(
+		'featured_category', array(
+			'type'        => 'select',
+			'section'     => 'static_front_page',
+			'label'       => __( 'Featured Category' ),
+			'description' => __( 'Category of posts displayed on the homepage' ),
+			'choices'     => getCatArray(),
+		)
+	);
 }
 
 add_action( 'customize_register', 'harwintonfair_customize_register' );
+
+function getCatArray() {
+	$cats = get_categories();
+	$resp = [];
+	foreach ( $cats as $cat ) {
+		$resp[ $cat->term_id ] = $cat->name;
+	}
+	return $resp;
+}
