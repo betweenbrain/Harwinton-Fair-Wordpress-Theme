d0674cb618a7580093bbed92d86f96519ee46c76
Matt Thomas <mthomas@guggenheim.org>
2019-07-06 18:12:19 -0400

    Reuse page template part

--- index.php
+++ index.php
@@ -8,14 +8,8 @@ get_header();
 
 if (have_posts()) : ?>
 	<main class="wrapper" role="main">
-		<?php while (have_posts()) :
-			the_post();
-			?>
-			<article>
-				<h1><?php the_title(); ?></h1>
-				<h4>Posted on <?php the_time('F jS, Y'); ?></h4>
-				<?php the_content(__('(more...)')); ?>
-			</article>
+		<?php while (have_posts()) : the_post(); ?>
+			<?php get_template_part('components/content', 'page'); ?>
 		<?php endwhile; ?>
 	</main>
 <?php endif; ?>
