2ceca607a65b34519ea30053270d8599b110eded
Matt Thomas <mthomas@guggenheim.org>
2019-07-06 18:07:48 -0400

    Wrap main default content area with main element and wrapper

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php
+
 /**
  * @package Harwintonfair
  */
 
 get_header();
 
-if (have_posts()) :
-	while (have_posts()) :
-		the_post();
-		?>
-		<article>
-			<h1><?php the_title(); ?></h1>
-			<h4>Posted on <?php the_time('F jS, Y'); ?></h4>
-			<?php the_content(__('(more...)')); ?>
-		</article>
-	<?php endwhile; ?>
+if (have_posts()) : ?>
+	<main class="wrapper" role="main">
+		<?php while (have_posts()) :
+			the_post();
+			?>
+			<article>
+				<h1><?php the_title(); ?></h1>
+				<h4>Posted on <?php the_time('F jS, Y'); ?></h4>
+				<?php the_content(__('(more...)')); ?>
+			</article>
+		<?php endwhile; ?>
+	</main>
 <?php endif; ?>
 <?php get_footer(); ?>
