19756f826099f36f8610e7be26132b79d937d7f1
Matt Thomas <mthomas@guggenheim.org>
2019-07-18 17:14:22 -0400

    Use post component

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
-
 /**
  * @package Harwintonfair
  */
 
 get_header();
 
+echo 'index.php' . "\n";
+global $wp;
+var_dump($wp->query_string);
+
 if (have_posts()) : ?>
 	<main class="wrapper" role="main">
 		<?php while (have_posts()) : the_post(); ?>
-			<?php get_template_part('components/content', 'page'); ?>
+			<?php get_template_part('components/post'); ?>
 		<?php endwhile; ?>
 	</main>
 <?php endif; ?>
