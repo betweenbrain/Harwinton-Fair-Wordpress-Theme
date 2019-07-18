dd813eabba17915d6c067ad9f3843a03d111c01e
Matt Thomas <mthomas@guggenheim.org>
2019-07-18 17:15:34 -0400

    Adds events archive template

---
+++ archive-events.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package Harwintonfair
+ */
+
+get_header();
+
+echo 'archive-events.php' . "\n";
+global $wp;
+var_dump($wp->query_string);
+
+if (have_posts()) : ?>
+	<main class="wrapper" role="main">
+		<?php while (have_posts()) : the_post(); ?>
+			<?php get_template_part('components/post'); ?>
+		<?php endwhile; ?>
+	</main>
+<?php endif; ?>
+<?php get_footer(); ?>
