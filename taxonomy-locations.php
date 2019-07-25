98c0d4f9d89da23f0f0bc4bd53f599b37ec8e63a
Matt Thomas <mthomas@guggenheim.org>
2019-07-25 13:21:46 -0400

    Implements location taxonomy template

---
+++ taxonomy-locations.php
@@ -0,0 +1,35 @@
+<?php
+get_header();
+
+$term = get_queried_object();
+$events = new WP_Query(
+    array(
+        'posts_per_page' => -1,
+        'post_type' => 'events',
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'locations',
+                'field' => 'term_id',
+                'terms' => $term->term_id,
+            )
+        )
+    )
+);
+$today = null;
+if ($events->have_posts()) : ?>
+    <main class="wrapper" role="main">
+        <h1>Events at the <?php echo $term->name ?></h1>
+
+        <?php while ($events->have_posts()) : $events->the_post(); ?>
+            <?php $meta = get_post_meta($post->ID); ?>
+            <?php
+            $date = date('l, F jS', strtotime($meta['begin'][0]));
+            if ($today != $date) {
+                $today = $date; ?>
+                <h2><?php echo $date ?></h2>
+            <?php    } ?>
+            <?php get_template_part('parts/event'); ?>
+        <?php endwhile; ?>
+    </main>
+<?php endif; ?>
+<?php get_footer(); ?>
