be79d441e7da4691148dcd9415e306886d46d3bd
Matt Thomas <mthomas@guggenheim.org>
2019-08-12 20:25:39 -0400

    Remove duplicate stylesheets

--- header.php
+++ header.php
@@ -7,8 +7,6 @@
 	<meta name="description" content="<?php echo strip_tags(get_the_excerpt()); ?>">
 	<meta name="author" content="<?php the_title(); ?>">
 	<?php wp_head(); ?>
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/normalize.css" type="text/css" media="all">
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all">
 	<?php
 	if ($bar_color = get_theme_mod('bar_color')) : ?>
 		<style type="text/css">
