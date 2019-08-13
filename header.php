15163037401e7935c1c1aa3f6848caa69247567c
Matt Thomas <mthomas@guggenheim.org>
2019-08-12 20:27:10 -0400

    Code style

--- header.php
+++ header.php
@@ -4,25 +4,28 @@
 <head>
 	<meta charset="utf-8">
 	<title><?php the_title(); ?></title>
-	<meta name="description" content="<?php echo strip_tags(get_the_excerpt()); ?>">
+	<meta name="description" content="<?php echo strip_tags( get_the_excerpt() ); ?>">
 	<meta name="author" content="<?php the_title(); ?>">
 	<?php wp_head(); ?>
 	<?php
-	if ($bar_color = get_theme_mod('bar_color')) : ?>
+	if ( $bar_color = get_theme_mod( 'bar_color' ) ) :
+		?>
 		<style type="text/css">
 			.bar {
-				background-color: <?php echo $bar_color;
-									?>;
-			}
+				background-color: 
+				<?php
+				echo $bar_color;
+				?>
+				}
 		</style>
-	<?php
+		<?php
 	endif;
 	?>
 </head>
 
 <body>
 	<?php
-	if ($description = get_bloginfo('description', 'display')) :
+	if ( $description = get_bloginfo( 'description', 'display' ) ) :
 		?>
 		<div class="headline bar top-bar" id="top">
 			<div class="wrapper">
@@ -48,25 +51,25 @@
 			</div>
 		</div>
 	<?php endif; ?>
-	<?php if (has_custom_logo() || is_active_sidebar('main_menu')) : ?>
+	<?php if ( has_custom_logo() || is_active_sidebar( 'main_menu' ) ) : ?>
 		<div class="sticky-header">
 			<div class="wrapper">
-				<?php if (has_custom_logo()) : ?>
+				<?php if ( has_custom_logo() ) : ?>
 					<?php the_custom_logo(); ?>
 				<?php endif; ?>
 
-				<?php if (is_active_sidebar('main_menu')) : ?>
+				<?php if ( is_active_sidebar( 'main_menu' ) ) : ?>
 					<nav class="main-navigation" id="menu" role="navigation">
 						<a href="#menu" aria-label="Open main menu" class="toggle open">Menu</a>
 						<a href="#top" aria-label="Close main menu" class="toggle close">Close</a>
-						<?php dynamic_sidebar('main_menu'); ?>
+						<?php dynamic_sidebar( 'main_menu' ); ?>
 					</nav>
 				<?php endif; ?>
 			</div>
 		</div>
 	<?php endif; ?>
-	<?php if (is_active_sidebar('call_to_action')) : ?>
+	<?php if ( is_active_sidebar( 'call_to_action' ) ) : ?>
 		<div class="call-to-action">
-			<?php dynamic_sidebar('call_to_action'); ?>
+			<?php dynamic_sidebar( 'call_to_action' ); ?>
 		</div>
 	<?php endif; ?>
