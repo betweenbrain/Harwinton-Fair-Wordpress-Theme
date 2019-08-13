41180966afcd5d34099ca3b234244043fbeccf73
Matt Thomas <mthomas@guggenheim.org>
2019-08-12 20:13:58 -0400

    Adds date and locations

--- parts/events.php
+++ parts/events.php
@@ -7,49 +7,61 @@
  *
  * @package Karuna
  */
+$meta      = get_post_meta( $post->ID );
+$locations = wp_get_post_terms( $post->ID, 'locations' );
+$date      = ( $meta['end'][0] )
+	? date( 'l, F jS', strtotime( $meta['begin'][0] ) ) . ' at ' . date( 'g:i A', strtotime( $meta['begin'][0] ) ) . ' - ' . date( 'g:i A', strtotime( $meta['end'][0] ) )
+	: date( 'l, F jS', strtotime( $meta['begin'][0] ) ) . ' at ' . date( 'g:i A', strtotime( $meta['begin'][0] ) );
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if (is_single() || is_page()) : ?>
+	<?php if ( is_single() || is_page() ) : ?>
 		<div class="post-thumbnail">
 			<?php the_post_thumbnail(); ?>
 		</div>
 	<?php endif; ?>
 	<header class="entry-header">
 		<?php
-		if (is_single()) {
-			the_title('<h1 class="entry-title">', '</h1>');
+		if ( is_single() ) {
+			the_title( '<h1 class="entry-title">', '</h1>' );
+			echo $date . ' at ' . $locations[0];
 		}
-		if (!is_single()) {
-			the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+		if ( ! is_single() ) {
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		}
 		?>
 	</header>
 	<div class="entry-content">
 		<?php
-		if (is_single()) {
-			the_content(sprintf(
-				wp_kses(__('Continue reading %s <span class="meta-nav">&rarr;</span>', 'harwintonfair'), array('span' => array('class' => array()))),
-				the_title('<span class="screen-reader-text">"', '"</span>', false)
-			));
+		if ( is_single() ) {
+			the_content(
+				sprintf(
+					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'harwintonfair' ), array( 'span' => array( 'class' => array() ) ) ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				)
+			);
 
-			wp_link_pages(array(
-				'before' => '<div class="page-links">' . esc_html__('Pages:', 'harwintonfair'),
-				'after'  => '</div>',
-			));
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'harwintonfair' ),
+					'after'  => '</div>',
+				)
+			);
 		}
-		if (!is_single()) {
-			$meta = get_post_meta($post->ID);
-			$terms = wp_get_post_terms($post->ID, 'locations');  ?>
-			<p><?php echo date('g:ia', strtotime($meta['begin'][0])); ?>
-				<?php if (isset($meta['end'])) { ?>
+		if ( ! is_single() ) {
+			?>
+			<p><?php echo date( 'g:ia', strtotime( $meta['begin'][0] ) ); ?>
+				<?php if ( isset( $meta['end'] ) ) { ?>
 					until
-					<?php echo date('g:ia', strtotime($meta['end'][0]));
-				} ?>
-				at <?php foreach ($terms as $term) : ?>
-					<a href="/location/<?php echo $term->slug ?>"><?php echo $term->name ?></a>
+					<?php
+					echo date( 'g:ia', strtotime( $meta['end'][0] ) );
+				}
+				?>
+				at <?php foreach ( $locations as $term ) : ?>
+					<a href="/location/<?php echo $term->slug; ?>"><?php echo $term->name; ?></a>
 				<?php endforeach; ?>
 			</p>
-			<?php the_excerpt();
+			<?php
+			the_excerpt();
 		}
 		?>
 	</div>
