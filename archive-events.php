668e2526644b69a77c8bdb856ae242351b11522b
Matt Thomas <mthomas@guggenheim.org>
2019-08-12 20:48:07 -0400

    Wrap each day in a section element

--- archive-events.php
+++ archive-events.php
@@ -5,30 +5,41 @@
 
 get_header();
 
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-$events = new WP_Query(array(
-	'post_type' => 'events',
-	'posts_per_page' => '50',
-	'paged' => $paged,
-	'order'      => 'ASC',
-	'orderby'   => 'meta_value',
-	'meta_key'  => 'begin',
-	'meta_type' => 'DATETIME'
-));
-$today = null;
+$paged  = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+$events = new WP_Query(
+	array(
+		'post_type'      => 'events',
+		'posts_per_page' => '50',
+		'paged'          => $paged,
+		'order'          => 'ASC',
+		'orderby'        => 'meta_value',
+		'meta_key'       => 'begin',
+		'meta_type'      => 'DATETIME',
+	)
+);
+$today  = null;
 
-if ($events->have_posts()) : ?>
+if ( $events->have_posts() ) : ?>
 	<main class="wrapper" role="main">
-		<?php while ($events->have_posts()) : $events->the_post(); ?>
-			<?php $meta = get_post_meta($post->ID); ?>
+		<section class="event-day">	
+		<?php while ( $events->have_posts() ) : ?>
 			<?php
-			$date = date('l, F jS', strtotime($meta['begin'][0]));
-			if ($today != $date) {
-				$today = $date; ?>
-				<h2><?php echo $date ?></h2>
-			<?php	} ?>
-			<?php get_template_part('parts/events'); ?>
+			$events->the_post();
+			$meta = get_post_meta( $post->ID );
+			?>
+			<?php
+			$date = date( 'l, F jS', strtotime( $meta['begin'][0] ) );
+			if ( $today != $date ) : ?>
+				<?php if ( ! is_null( $today ) ) : ?>
+				</section>
+				<section class="event-day">
+			<?php endif; ?>
+				<?php $today = $date; ?>
+				<h2><?php echo $date; ?></h2>
+				<?php endif; ?>
+			<?php get_template_part( 'parts/events' ); ?>
 		<?php endwhile; ?>
+		</section>
 	</main>
 <?php endif; ?>
 <?php get_footer(); ?>
