be99b4149464ad4b856963b00ac0c43aa22add52
Matt Thomas <mthomas@guggenheim.org>
2019-08-13 20:00:51 -0400

    Adds viewport meta tag

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php the_title(); ?></title>
 	<meta name="description" content="<?php echo strip_tags( get_the_excerpt() ); ?>">
 	<meta name="author" content="<?php the_title(); ?>">
