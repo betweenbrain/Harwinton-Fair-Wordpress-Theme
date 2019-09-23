f60b4dba75223ae3b6f353585b157299dc848b7e
Matt Thomas <mthomas@guggenheim.org>
2019-09-23 14:39:35 -0400

    Display today's fair program on the homepage

--- front-page.php
+++ front-page.php
@@ -2,7 +2,6 @@
 /**
  * @package Harwintonfair
  */
-
 get_header(); ?>
 <div id="primary" class="content-area">
 	<?php
@@ -35,11 +34,48 @@ get_header(); ?>
 	<main class="wrapper
 	<?php
 	if ( is_active_sidebar( 'sidebar' ) ) {
-		echo ' two-column';}
-	?>
-	" role="main">
-		<div>
+		echo ' two-column';} ?>" role="main">
+	<div>
 		<?php
+		/**
+		 * Show today's program if any.
+		 */
+		$today = date( 'Y-m-d' );
+		$query = new WP_Query(
+			array(
+				'post_type'      => 'program',
+				'posts_per_page' => '50',
+				'paged'          => '1',
+				'order'          => 'ASC',
+				'orderby'        => 'meta_value',
+				'meta_key'       => 'begin',
+				'meta_type'      => 'DATETIME',
+				'meta_compare'   => 'LIKE',
+				'meta_value'     => $today,
+			)
+		);
+		if ( $query->have_posts() ) : ?>
+		<div>
+			<h1>Today's Fair Program - <?php echo date( 'l, F jS', strtotime( $today ) );?></h1>
+			<?php
+			while ( $query->have_posts() ) :
+				$query->the_post();
+				?>
+				<section class="event-day">
+				<ol>
+					<?php get_template_part( 'parts/program-list-item' ); ?>
+				</ol>
+			</section>
+				<?php endwhile; ?>
+			</div>
+			<?php
+		endif;
+		wp_reset_postdata();
+		?>
+		<?php
+		/**
+		 * Show sticky posts if any.
+		 */
 		$category = get_theme_mod( 'featured_category' );
 		$query    = new WP_Query(
 			array(
@@ -48,14 +84,17 @@ get_header(); ?>
 				'ignore_sticky_posts' => 1,
 			)
 		);
-		if ( $query->have_posts() ) {
+		if ( $query->have_posts() ) : ?>
+		<div>
+			<?php
 			while ( $query->have_posts() ) {
 				$query->the_post();
 				echo get_template_part( 'parts/page' );
 			}
-		}
-		wp_reset_postdata();
-		?>
+			?>
+		</div>
+		<?php endif;
+		wp_reset_postdata(); ?>
 		</div>
 		<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
 		<aside>
